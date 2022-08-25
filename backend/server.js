ad091492bf7de8a2dfa86c49ef7ab48d79218673
Roymaniac <amolojoseph2020@gmail.com>
2022-08-25 14:00:33 +0100

    Database connection & error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require("express"); // Express package
+const colors = require("colors")
 const dotenv = require("dotenv").config(); // Environment variable
-
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB  = require("./config/db")
 const PORT = process.env.PORT || 5000; // PORT
+
+connectDB() // DB config
 
 const app = express(); // Initialize Express
 
+const goalRoutes = require("./routes/goalRoutes"); // GoalRoutes import
+
+// Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", goalRoutes);
+
+// custom error handle middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // Server
